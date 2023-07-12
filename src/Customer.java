d4d079d7e22545166458699f041251d347cc8786
Kavya N <kavyabhushan2017@gmail.com>
2023-07-12 12:46:54 +0530

    initial commit

---
+++ src/Customer.java
@@ -0,0 +1,38 @@
+public class Customer {
+
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public Customer(String name,double creditLimit,String email){
+        this.name=name;
+        this.creditLimit=creditLimit;
+        this.email=email;
+    }
+    public String getName(){
+    return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public double getcreditLimit(){
+        return creditLimit;
+    }
+
+    public void setcreditLimit(double creditName){
+        this.creditLimit=creditName;
+
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+}
+
---
+++ src/CustomerMainClass.java
@@ -0,0 +1,11 @@
+public class CustomerMainClass {
+
+    public static void main(String[] args){
+
+        Customer c=new Customer("A",100.0, "a@gmail.com");
+
+        System.out.println(c.getName());
+        c.setName("JAVA");
+        System.out.println(c.getName());
+    }
+}
